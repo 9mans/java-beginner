323354b6bc71bf5bc0b5679b576a0b8844a84ff3
9mans <rbska1234@naver.com>
2024-09-18 15:57:29 +0900

    about construct

---
+++ src/construct/MemberConstruct.java
@@ -0,0 +1,16 @@
+package construct;
+
+public class MemberConstruct {
+
+    String name;
+    int age;
+    int grade;
+
+
+    MemberConstruct(String name, int age, int grade) {
+
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+}
