8f5495732c5f92689032527cf752aa568cd38e6c
9mans <rbska1234@naver.com>
2024-09-18 16:08:20 +0900

    about construct

--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -13,4 +13,14 @@ public class MemberConstruct {
         this.age = age;
         this.grade = grade;
     }
+
+    MemberConstruct(String name, int age) {
+        // 첫번째 줄에서만 this 사용가능 
+        this(name, age, 50);
+//        this.name = name;
+//        this.age = age;
+//        this.grade = 50;
+    }
+
+
 }
